bf257e38b5954ba42341f3d339f9a80a7bc3fae7
josiethomas1 <157053272+josiethomas1@users.noreply.github.com>
2024-04-30 09:49:10 -0600

    js

--- finalJS.js
+++ finalJS.js
@@ -1,27 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const volumeIndicator = document.getElementById("volumeIndicator");
-  const volumeUpButton = document.getElementById("volumeUpButton");
-  const volumeDownButton = document.getElementById("volumeDownButton");
-  let volumeLevel = 50;
-
-  function updateVolumeIndicator() {
-      volumeIndicator.style.height = volumeLevel + "%"; //This function adjusts the visual representation of volume
-  }
-
-  function increaseVolume() {
-      if (volumeLevel < 100) {
-          volumeLevel += 10;
-          updateVolumeIndicator();
-      }
-  }
-
-  function decreaseVolume() {
+    const volumeIndicator = document.getElementById("volumeIndicator");
+    const volumeBar = document.getElementById("volumeBar");
+    const volumeUpButton = document.getElementById("volumeUpButton");
+    const volumeDownButton = document.getElementById("volumeDownButton");
+    let volumeLevel = 50;
+  
+    function updateVolumeIndicator() {
+      volumeIndicator.style.height = volumeLevel + "%"; // This function adjusts the visual representation of volume
+      volumeBar.style.height = (100 - volumeLevel) + "%";
+    }
+  
+    function increaseVolume() {
       if (volumeLevel > 0) {
-          volumeLevel -= 10;
-          updateVolumeIndicator();
+        volumeLevel -= 10;
+        updateVolumeIndicator();
       }
-  }
-
-  volumeUpButton.addEventListener("click", increaseVolume);
-  volumeDownButton.addEventListener("click", decreaseVolume);
-});
+    }
+  
+    function decreaseVolume() {
+      if (volumeLevel < 100) {
+        volumeLevel += 10;
+        updateVolumeIndicator();
+      }
+    }
+  
+    volumeUpButton.addEventListener("click", decreaseVolume);
+    volumeDownButton.addEventListener("click", increaseVolume);
+  });
+  
